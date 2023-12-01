b27793d85a68ef5a39cca5aa6fda401fdea2940d
MagmaMC <magma.mc@magma-mc.net>
2023-12-01 20:01:40 +0000

    1.0.0

--- Runtime/ForceStay.cs
+++ Runtime/ForceStay.cs
@@ -2,33 +2,31 @@
 using UnityEngine;
 using VRC.SDKBase;
 
-public class ForceStay : UdonSharpBehaviour
+public class ForceStay: UdonSharpBehaviour
 {
     [Header("Util For Keeping Players In One Spot")]
     public uint MaxDistance = 15;
     public bool RemoveVelocity = true;
     public bool SetRotation = true;
+
     void FixedUpdate()
     {
-        if (Vector3.Distance(Networking.LocalPlayer.GetPosition(), transform.position) > MaxDistance)
+        VRCPlayerApi LocalPlayer = Networking.LocalPlayer;
+
+        if (Vector3.Distance(LocalPlayer.GetPosition(), transform.position) > MaxDistance)
         {
-            Teleport(Networking.LocalPlayer, transform.position, (SetRotation ? transform.rotation : Networking.LocalPlayer.GetRotation()), true);
+            Quaternion TeleportRot = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+            Quaternion InvPlayerRot = Quaternion.Inverse(Networking.LocalPlayer.GetRotation());
+            VRCPlayerApi.TrackingData origin = LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Origin);
+
+            Vector3 targetPos = transform.position + TeleportRot * InvPlayerRot * (origin.position - LocalPlayer.GetPosition());
+            Quaternion targetRot = TeleportRot * (InvPlayerRot * origin.rotation);
+
             if (RemoveVelocity)
-                Networking.LocalPlayer.SetVelocity(Vector3.zero);
+                LocalPlayer.SetVelocity(Vector3.zero);
+
+            LocalPlayer.TeleportTo(targetPos, targetRot, VRC_SceneDescriptor.SpawnOrientation.AlignRoomWithSpawnPoint, true);
+
         }
-    }
-
-    public static void Teleport(VRCPlayerApi player, Vector3 teleportPos, Quaternion teleportRot, bool lerpOnRemote = false)
-    {
-        teleportRot = Quaternion.Euler(0, teleportRot.eulerAngles.y, 0);
-
-        Quaternion invPlayerRot = Quaternion.Inverse(player.GetRotation());
-
-        VRCPlayerApi.TrackingData origin = player.GetTrackingData(VRCPlayerApi.TrackingDataType.Origin);
-
-        Vector3 targetPos = teleportPos + teleportRot * invPlayerRot * (origin.position - player.GetPosition());
-        Quaternion targetRot = teleportRot * (invPlayerRot * origin.rotation);
-
-        player.TeleportTo(targetPos, targetRot, VRC_SceneDescriptor.SpawnOrientation.AlignRoomWithSpawnPoint, lerpOnRemote);
     }
 }
